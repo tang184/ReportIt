c5691920381b85a320faeb5500ae3a212bd9dfd8
gundam1996 <chen1631@purdue.edu>
2017-09-18 16:13:30 -0400

    Added basic template for  Concern submission module Added 404 Page Added migration file to gitignore

--- reportit/webpage/admin.py
+++ reportit/webpage/admin.py
@@ -6,14 +6,22 @@ from django.contrib import admin
 
 from .models import ReporterProfile
 from .models import AgentProfile
+<<<<<<< db71c15b78e45736b7b03f84df552ec378016b8e
 from .models import Agent
 from .models import Concern
 
 admin.site.register(ReporterProfile)
 admin.site.register(AgentProfile)
 admin.site.register(Agent)
+=======
+from .models import Concern
+>>>>>>> Added basic template for  Concern submission module
 
 
 admin.site.register(ReporterProfile)
 admin.site.register(AgentProfile)
+<<<<<<< db71c15b78e45736b7b03f84df552ec378016b8e
 admin.site.register(Concern)
+=======
+admin.site.register(Concern)
+>>>>>>> Added basic template for  Concern submission module
