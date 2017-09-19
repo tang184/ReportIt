735f45e09463c9294862f4d2acc431bea1bbcd90
Liang Zhang <506425878@qq.com>
2017-09-18 21:01:00 -0400

    logout function, remove helptext for signup

--- reportit/webpage/forms.py
+++ reportit/webpage/forms.py
@@ -7,8 +7,31 @@ class ReporterSignUpForm(UserCreationForm):
     #last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254)
 
+    def __init__(self, *args, **kwargs):
+        super(ReporterSignUpForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class AgentSignUpForm(UserCreationForm):
+    #first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    #last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254)
+
+    def __init__(self, *args, **kwargs):
+        super(AgentSignUpForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
 
 
--- reportit/webpage/views.py
+++ reportit/webpage/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
-from .forms import ReporterSignUpForm
+from .forms import ReporterSignUpForm, AgentSignUpForm
 
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
@@ -37,16 +37,29 @@ def reporterSignup(request):
 
             username = form.cleaned_data.get('username')
             print (username)
-            #print(username)
             password1 = form.cleaned_data.get('password1')
-            #print(password1)
             password2 = form.cleaned_data.get('password2')
-            #user = authenticate(username=username, password=password1)
             form.save()
             return redirect('/')
     else:
         form = ReporterSignUpForm()
     return render(request, 'webpage/reporterSignup.html', {'form': form})
+
+def agentSignup(request):
+    if request.method == 'POST':
+        form = AgentSignUpForm(request.POST)
+        #print(form)
+        if form.is_valid():
+
+            username = form.cleaned_data.get('username')
+            print (username)
+            password1 = form.cleaned_data.get('password1')
+            password2 = form.cleaned_data.get('password2')
+            form.save()
+            return redirect('/')
+    else:
+        form = AgentSignUpForm()
+    return render(request, 'webpage/agentSignup.html', {'form': form})
 
 def viewProfile(request):
     return render(request, 'webpage/profile.html')
