9b48642b0e704966d893007d4abbe3016e7c798a
Liang Zhang <506425878@qq.com>
2017-09-28 01:31:34 -0400

    implement forget password function

--- reportit/reportit/settings.py
+++ reportit/reportit/settings.py
@@ -124,3 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#enable email function
+if DEBUG:
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_HOST_USER = 'johnnyforproject@gmail.com'
+    EMAIL_HOST_PASSWORD = '19941214'
+    EMAIL_USE_TLS = True
+    DEFAULT_FROM_EMAIL = 'ReportIt Team'
