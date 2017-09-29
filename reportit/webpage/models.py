d46fdf19b0bf98d7ddd377b972e34fd9852cfc3d
gundam1996 <chen1631@purdue.edu>
2017-09-29 15:58:26 -0400

    Make legal_name not required

--- reportit/webpage/models.py
+++ reportit/webpage/models.py
@@ -10,6 +10,7 @@ from webpage.validators import validateURL, validateEmail
 
 class Reporter(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    legal_name = models.CharField(verbose_name='Legal Name', max_length=100)
     gender = models.CharField(max_length = 10, blank=True, null=True)
     phone_number = models.CharField(verbose_name='Phone number(Optional)', max_length=100, blank=True, null=True)
     address = models.CharField(verbose_name='Address(Optional)', max_length=300, blank=True, null=True)
@@ -21,7 +22,7 @@ class Reporter(models.Model):
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    legal_name = models.CharField(verbose_name='Legal Name', max_length=100)
+    legal_name = models.CharField(blank=False,default=False, verbose_name='Legal Name', max_length=100)
     phone_number = models.CharField(verbose_name='Phone number', max_length=100)
     address = models.CharField(verbose_name='Address', max_length=300)
     agentimage = models.CharField(verbose_name='Agent Badge Logo', max_length=300, validators=[validateURL], default=None)
